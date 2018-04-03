8adf18bdf20a21ff153fc38efc43b4a99c54edf4
suniljackie <31463813+suniljackie@users.noreply.github.com>
2018-04-03 23:19:11 +0530

    secound commit

--- src/main/java/Com/j/javaTest.java
+++ src/main/java/Com/j/javaTest.java
@@ -1,16 +1,25 @@
 package Com.j;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class javaTest {
 
+	WebDriver d;
+
 	@Test
 	public void set() {
 		//
 		System.setProperty("webdriver.chrome.driver", "D:\\MS OFFICE\\SELENIUM\\chromedriver.exe");
-		ChromeDriver d = new ChromeDriver();
+		d = new ChromeDriver();
 		d.get("https://jqueryui.com/resources/demos/datepicker/default.html");
 		d.manage().window().maximize();
+	}
+
+	@Test
+	public void close() {
+		System.out.println("succesfully closed");
+		d.close();
 	}
 }
